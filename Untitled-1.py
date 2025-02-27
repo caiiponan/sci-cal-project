2caf3d346464170ebc964e04dfb8f18fe22f6171
caiponan <carlosalberto.ponan.04@gmail.com>
2025-02-27 19:22:05 +0800

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -11,6 +11,57 @@ root.geometry("480x568+0+0")
 calc = Frame(root)
 calc.grid()
 
+txtDisplay = Entry(calc, font = ('arial', 20, 'bold'), bg = "powder blue", bd = 30, width = 28, justify = RIGHT)
+txtDisplay.grid(row = 0, column = 0, columnspan = 4, pady = 1)
+txtDisplay.insert(0, "0")
+
+numberpad = "789456123"
+i = 0
+btn = []
+for j in range(2, 5):
+    for k in range(3):
+        btn.append(Button(calc, width = 6, height = 2, font = ('arial', 20, 'bold'), bd = 4, text = numberpad[i]))
+        btn[i].grid(row = j, column = k, pady = 1)
+        i += 1
+
+btnClear = Button(calc, text = chr(67), width = 6, height = 2, font = ('arial', 20, 'bold'), bd = 4, bg = "orange").grid(row = 1, column = 0, pady = 1)
+btnAllClear = Button(calc, text = chr(67) + chr(69), width = 6, height = 2, font = ('arial', 20, 'bold'), bd = 4, bg = "orange").grid(row = 1, column = 1, pady = 1)
+btnSqrt = Button(calc, text = "√", width = 6, height = 2, font = ('arial', 20, 'bold'), bd = 4, bg = "powder blue").grid(row = 1, column = 2, pady = 1)
+btnAdd = Button(calc, text = "+", width = 6, height = 2, font = ('arial', 20, 'bold'), bd = 4, bg = "powder blue").grid(row = 1, column = 3, pady = 1)
+btnSub = Button(calc, text = "-", width = 6, height = 2, font = ('arial', 20, 'bold'), bd = 4, bg = "powder blue").grid(row = 2, column = 3, pady = 1)
+btnMulti = Button(calc, text = "×", width = 6, height = 2, font = ('arial', 20, 'bold'), bd = 4, bg = "powder blue").grid(row = 3, column = 3, pady = 1)
+btnDiv = Button(calc, text = "÷", width = 6, height = 2, font = ('arial', 20, 'bold'), bd = 4, bg = "powder blue").grid(row = 4, column = 3, pady = 1)
+btnZero = Button(calc, text = "0", width = 6, height = 2, font = ('arial', 20, 'bold'), bd = 4, bg = "powder blue").grid(row = 5, column = 0, pady = 1)
+btnDot = Button(calc, text = ".", width = 6, height = 2, font = ('arial', 20, 'bold'), bd = 4, bg = "powder blue").grid(row = 5, column = 1, pady = 1)
+btnPM = Button(calc, text = chr(177), width = 6, height = 2, font = ('arial', 20, 'bold'), bd = 4, bg = "powder blue").grid(row = 5, column = 2, pady = 1)
+btnEqual = Button(calc, text = "=", width = 6, height = 2, font = ('arial', 20, 'bold'), bd = 4, bg = "powder blue").grid(row = 5, column = 3, pady = 1)
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Sci-cal Part~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+btnPi = Button(calc, text = "π", width = 6, height = 2, font = ('arial', 20, 'bold'), bd = 4, bg = "powder blue").grid(row = 1, column = 4, pady = 1)
+btnCos = Button(calc, text = "cos", width = 6, height = 2, font = ('arial', 20, 'bold'), bd = 4, bg = "powder blue").grid(row = 1, column = 5, pady = 1)
+btnTan = Button(calc, text = "tan", width = 6, height = 2, font = ('arial', 20, 'bold'), bd = 4, bg = "powder blue").grid(row = 1, column = 6, pady = 1)
+btnSin = Button(calc, text = "sin", width = 6, height = 2, font = ('arial', 20, 'bold'), bd = 4, bg = "powder blue").grid(row = 1, column = 7, pady = 1)
+
+btn2Pi = Button(calc, text = "2π", width = 6, height = 2, font = ('arial', 20, 'bold'), bd = 4, bg = "powder blue").grid(row = 2, column = 4, pady = 1)
+btnCosh = Button(calc, text = "cosh", width = 6, height = 2, font = ('arial', 20, 'bold'), bd = 4, bg = "powder blue").grid(row = 2, column = 5, pady = 1)
+btnTanh = Button(calc, text = "tanh", width = 6, height = 2, font = ('arial', 20, 'bold'), bd = 4, bg = "powder blue").grid(row = 2, column = 6, pady = 1)
+btnSinh = Button(calc, text = "sinh", width = 6, height = 2, font = ('arial', 20, 'bold'), bd = 4, bg = "powder blue").grid(row = 2, column = 7, pady = 1)
+
+btnLog = Button(calc, text = "log", width = 6, height = 2, font = ('arial', 20, 'bold'), bd = 4, bg = "powder blue").grid(row = 3, column = 4, pady = 1)
+btnExp = Button(calc, text = "Exp", width = 6, height = 2, font = ('arial', 20, 'bold'), bd = 4, bg = "powder blue").grid(row = 3, column = 5, pady = 1)
+btnMod = Button(calc, text = "Mod", width = 6, height = 2, font = ('arial', 20, 'bold'), bd = 4, bg = "powder blue").grid(row = 3, column = 6, pady = 1)
+btnE = Button(calc, text = "e", width = 6, height = 2, font = ('arial', 20, 'bold'), bd = 4, bg = "powder blue").grid(row = 3, column = 7, pady = 1)
+
+btnLog2 = Button(calc, text = "log2", width = 6, height = 2, font = ('arial', 20, 'bold'), bd = 4, bg = "powder blue").grid(row = 4, column = 4, pady = 1)
+btnDeg = Button(calc, text = "deg", width = 6, height = 2, font = ('arial', 20, 'bold'), bd = 4, bg = "powder blue").grid(row = 4, column = 5, pady = 1)
+btnAcosh = Button(calc, text = "acosh", width = 6, height = 2, font = ('arial', 20, 'bold'), bd = 4, bg = "powder blue").grid(row = 4, column = 6, pady = 1)
+btnAsinh = Button(calc, text = "asinh", width = 6, height = 2, font = ('arial', 20, 'bold'), bd = 4, bg = "powder blue").grid(row = 4, column = 7, pady = 1)
+
+btnLog10 = Button(calc, text = "log10", width = 6, height = 2, font = ('arial', 20, 'bold'), bd = 4, bg = "powder blue").grid(row = 5, column = 4, pady = 1)
+btnLog1p = Button(calc, text = "log1p", width = 6, height = 2, font = ('arial', 20, 'bold'), bd = 4, bg = "powder blue").grid(row = 5, column = 5, pady = 1)
+btnExpm1 = Button(calc, text = "expm1", width = 6, height = 2, font = ('arial', 20, 'bold'), bd = 4, bg = "powder blue").grid(row = 5, column = 6, pady = 1)
+btnLgamma = Button(calc, text = "lgamma", width = 6, height = 2, font = ('arial', 20, 'bold'), bd = 4, bg = "powder blue").grid(row = 5, column = 7, pady = 1)
 
 def iExit():
     iExit = tkinter.messagebox.askyesno("Scientific Calculator", "Confirm if you want to exit")
